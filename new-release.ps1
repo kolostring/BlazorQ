071c68b4d5b0ff259dabba0d4112685c22f6fb0f
kolostring <yoerodriguezjunior@gmail.com>
2026-03-11 10:20:35 -0400

    chore: remove old script for version releasing

--- new-release.ps1
+++
@@ -1,9 +0,0 @@
-﻿# 1. Clean old builds
-Remove-Item -Recurse -Force ./bin/Release
-
-# 2. Pack with a unique timestamp version to bypass cache
-$ver = "0.1.0-dev." + (Get-Date -Format "yyMMddHHmm")
-dotnet pack -c Release /p:Version=$ver
-
-# 3. Push to your local folder
-dotnet nuget push bin/Release/*.nupkg --source C:\NugetTest
