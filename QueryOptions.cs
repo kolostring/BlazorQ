37fdd22de5eabb999262b77be676745ed1f747c6
kolostring <yoerodriguezjunior@gmail.com>
2026-03-16 09:56:28 -0400

    feat: add Builder to pattern to QueryOptionsFactory

--- QueryOptions.cs
+++ QueryOptions.cs
@@ -4,16 +4,68 @@ namespace BlazorQ;
 
 public static class QueryOptionsFactory
 {
-    public static QueryOptions<TKey, TRes> Create<TKey, TRes>(
+    public static QueryOptionsBuilder<TKey, TRes> Create<TKey, TRes>(
         TKey key,
-        Func<QueryHandlerExecutionContext<TKey>, Task<QueryResult<TRes>>> handler,
-        Func<TRes, Task>? onSuccess = null,
-        Func<QueryError, Task>? onError = null,
-        Func<QueryResult<TRes>, Task>? onSettled = null,
-        bool enabled = true,
-        FetchOptions? fetchOptions = null,
-        CacheOptions? cacheOptions = null) where TKey : ITuple
-        => new(key, handler, onSuccess, onError, onSettled, enabled);
+        Func<QueryHandlerExecutionContext<TKey>, Task<QueryResult<TRes>>> handler)
+        where TKey : ITuple
+        => new(key, handler);
+}
+
+public sealed class QueryOptionsBuilder<TKey, TRes>(
+    TKey key,
+    Func<QueryHandlerExecutionContext<TKey>, Task<QueryResult<TRes>>> handler)
+    where TKey : ITuple
+{
+    private Func<TRes, Task>? _onSuccess;
+    private Func<QueryError, Task>? _onError;
+    private Func<QueryResult<TRes>, Task>? _onSettled;
+    private bool _enabled = true;
+    private FetchOptions _fetchOptions = new();
+    private CacheOptions? _cacheOptions;
+
+    public QueryOptionsBuilder<TKey, TRes> OnSuccess(Func<TRes, Task> action)
+    {
+        _onSuccess = action;
+        return this;
+    }
+
+    public QueryOptionsBuilder<TKey, TRes> OnError(Func<QueryError, Task> action)
+    {
+        _onError = action;
+        return this;
+    }
+    
+    public QueryOptionsBuilder<TKey, TRes> OnSettled(Func<QueryResult<TRes>, Task> action)
+    {
+        _onSettled = action;
+        return this;
+    }
+
+    public QueryOptionsBuilder<TKey, TRes> SetEnabled(bool enabled)
+    {
+        _enabled = enabled;
+        return this;
+    }
+
+    public QueryOptionsBuilder<TKey, TRes> WithFetchOptions(FetchOptions fetchOptions)
+    {
+        _fetchOptions = fetchOptions;
+        return this;
+    }
+
+    public QueryOptions<TKey, TRes> Build() => new(
+        key,
+        handler,
+        _onSuccess,
+        _onError,
+        _onSettled,
+        _enabled,
+        _fetchOptions,
+        _cacheOptions
+    );
+
+    public static implicit operator QueryOptions<TKey, TRes>(QueryOptionsBuilder<TKey, TRes> builder)
+        => builder.Build();
 }
 
 public sealed record QueryOptions<TKey, TRes>(
